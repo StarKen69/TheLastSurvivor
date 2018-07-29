5cda72a625c97f25f3bdc5980d06e43e3ed76d36
StarKen69 <karenymarcelo0606@gmail.com>
2018-07-29 20:00:11 +0200

    Proyecto
    Se agrego la carpeta principal del proyecto.

---
+++ The Last survivor/Assets/Scripts/Camera_Prueba.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Prueba : MonoBehaviour {
+
+    #region // variables publicas
+
+    public Camera CPcamera;
+    public float VelocidadVertical_Y;
+
+    float V;
+
+
+    public enum RotationAxes { MouseXAnddY = 0, MouseX = 1, MouseY = 2 };
+    public RotationAxes axes = RotationAxes.MouseXAnddY;
+    public float sensitivityX = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    float rotationX = 0F;
+
+    Quaternion originalRotation;
+
+    #endregion
+
+    #region // region start
+
+    void Start () {
+
+        {
+            // Make the rigid body not change rotation
+            if (GetComponent<Rigidbody>())
+                GetComponent<Rigidbody>().freezeRotation = true;
+            originalRotation = transform.localRotation;
+        }
+
+    }
+
+    #endregion
+
+    #region // region de controles (W,S,A,D)
+
+    void Update () {
+
+        V = VelocidadVertical_Y * Input.GetAxis("Mouse Y");
+
+        CPcamera.transform.Rotate(-V, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0, 0, -0.1f);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Translate(-0.1f, 0, 0);
+                }
+                else
+                {
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        transform.Translate(0.1f, 0, 0);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+    #region // region rotamiento de camara (Eje Y,Z)
+        if (axes == RotationAxes.MouseXAnddY)
+        {
+
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
+        }
+        else
+        {
+
+
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+    #endregion
+}
+
+
---
+++ The Last survivor/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MouseLook : MonoBehaviour
+{
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 };
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityY = 15F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    Quaternion originalRotation;
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            // Read the mouse input axis
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+            transform.localRotation = originalRotation * yQuaternion;
+        }
+    }
+
+    void Start()
+    {
+        // Make the rigid body not change rotation
+        if (GetComponent<Rigidbody>())
+            GetComponent<Rigidbody>().freezeRotation = true;
+        originalRotation = transform.localRotation;
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
