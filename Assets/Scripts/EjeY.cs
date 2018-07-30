d88ed581a885919b5902050dff3437f7df52b8f9
BuildTools <edgar.garcia.zafra@gmail.com>
2018-07-30 15:13:34 +0200

    Removing all files in .gitignore

---
+++ Assets/Scripts/EjeY.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjeY : MonoBehaviour {
+
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 };
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityY = 15F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    Quaternion originalRotation;
+
+
+
+    void Start () {
+		
+
+
+
+
+
+	}
+	
+
+
+
+
+
+
+
+	void Update () {
+		
+
+
+
+
+
+	}
+}
