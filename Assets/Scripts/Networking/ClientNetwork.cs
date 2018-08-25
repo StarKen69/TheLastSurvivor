98688e5d99a0b3bf98090b001c293db086123756
BuildTools <edgar.garcia.zafra@gmail.com>
2018-08-26 01:47:56 +0200

    Added basic networking (testing)

---
+++ Assets/Scripts/Networking/ClientNetwork.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class ClientNetwork : MonoBehaviour {
+    private NetworkClient myClient;
+
+    public void Start()
+    {
+        StartClient();
+    }
+
+    public void StartClient()
+    {
+        myClient = new NetworkClient();
+        myClient.RegisterHandler(MsgType.Connect, OnConnected);
+        myClient.RegisterHandler(MsgType.Disconnect, OnDisconnected);
+        myClient.Connect("127.0.0.1", ServerNetwork.SERVER_PORT);
+    }
+
+    public void Disconnect()
+    {
+        myClient.Disconnect();
+    }
+
+    private void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Connected to server successfully!");
+    }
+
+    private void OnDisconnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Disconnected from server");
+    }
+}
---
+++ Assets/Scripts/Networking/ServerNetwork.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerNetwork : MonoBehaviour {
+    public const int SERVER_PORT = 4444;
+
+    public GameObject startServerBtn, stopServerBtn;
+
+    public void StartServer()
+    {
+        if(NetworkServer.Listen(SERVER_PORT))
+        {
+            Debug.Log("Server started at port " + SERVER_PORT);
+            startServerBtn.SetActive(false);
+            stopServerBtn.SetActive(true);
+        }
+    }
+
+    public void StopServer()
+    {
+        NetworkServer.Shutdown();
+        Debug.Log("Server stopped");
+
+        startServerBtn.SetActive(true);
+        stopServerBtn.SetActive(false);
+    }
+}
